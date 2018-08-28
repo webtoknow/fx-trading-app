27414e409f666abc78e1e257cb38f5cb856bf30d
Dana Munteanu <dana.maria.munteanu@gmail.com>
2018-08-28 17:22:33 +0300

    ui - add router and pages

--- App/UI/src/app/app.module.ts
+++ App/UI/src/app/app.module.ts
@@ -3,11 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register',component: RegisterPageComponent },
+  { path: 'dashboard', component: DashboardPageComponent },
+  { path: '**', component: NotFoundPageComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    DashboardPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
