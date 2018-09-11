4ebc8be9329eb7df4b11fd4a4dc5e7b3febbf74f
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2018-09-11 08:56:09 +0300

    Updated Blotter view

---
+++ App/ui/src/app/models/transaction.ts
@@ -0,0 +1,8 @@
+export interface Transaction {
+  dealId: number;
+  username: string;
+  CCY: string;
+  notional: string;
+  tenor: string;
+  date: number;
+}
--- App/ui/src/app/pages/dashboard-page/blotter-view/blotter-view.component.ts
+++ App/ui/src/app/pages/dashboard-page/blotter-view/blotter-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Transaction } from 'src/app/models/transaction';
 
 @Component({
   selector: 'app-blotter-view',
@@ -6,9 +7,121 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blotter-view.component.css']
 })
 export class BlotterViewComponent implements OnInit {
-
+  tranzactions: Transaction[] = [
+    {
+      dealId: 1234,
+      username: "Catalin Popescu",
+      CCY: "USD",
+      notional: "1000000",
+      tenor: "1M",
+      date: 1536606799000
+    },
+    {
+      dealId: 212,
+      username: "Bogdan Pascu",
+      CCY: "USD",
+      notional: "1000000",
+      tenor: "1M",
+      date: 1536606771000
+    },
+    {
+      dealId: 34,
+      username: "George Popescu",
+      CCY: "EUR",
+      notional: "1000000",
+      tenor: "3M",
+      date: 1536605766000
+    },
+    {
+      dealId: 455,
+      username: "Andrei Nare",
+      CCY: "USD",
+      notional: "1000000",
+      tenor: "1M",
+      date: 1536605766000
+    },
+    {
+      dealId: 55,
+      username: "George Barbu",
+      CCY: "RON",
+      notional: "1000000",
+      tenor: "1M",
+      date: 1536605766000
+    },
+    {
+      dealId: 3133,
+      username: "Maria Munteanu",
+      CCY: "USD",
+      notional: "1000000",
+      tenor: "1M",
+      date: 1536605754000
+    },
+    {
+      dealId: 3123,
+      username: "Mihai Nedelcu",
+      CCY: "EUR",
+      notional: "1000000",
+      tenor: "1M",
+      date: 1536605754000
+    },
+    {
+      dealId: 4523,
+      username: "Dan Purcarete",
+      CCY: "USD",
+      notional: "1000000",
+      tenor: "1M",
+      date: 1536605754000
+    },
+    {
+      dealId: 123,
+      username: "Ileana Geo",
+      CCY: "RON",
+      notional: "1000000",
+      tenor: "3M",
+      date: 1536605747000
+    },
+    {
+      dealId: 12,
+      username: "Dana Popescu",
+      CCY: "EUR",
+      notional: "1000000",
+      tenor: "1M",
+      date: 1536605743000
+    },
+    {
+      dealId: 1,
+      username: "George Nicolae",
+      CCY: "USD",
+      notional: "234324",
+      tenor: "1M",
+      date: 1536605738000
+    },
+    {
+      dealId: 1234,
+      username: "Piti Popescu",
+      CCY: "USD",
+      notional: "10050000",
+      tenor: "1M",
+      date: 1536605728000
+    },
+    {
+      dealId: 55,
+      username: "Adriana Popescu",
+      CCY: "USD",
+      notional: "1234",
+      tenor: "1M",
+      date: 1536605724000
+    },
+    {
+      dealId: 5234,
+      username: "George Marcu",
+      CCY: "USD",
+      notional: "55523",
+      tenor: "3M",
+      date: 1536604812000
+    },
+  ]
   constructor() { }
-
   ngOnInit() {
   }
 
