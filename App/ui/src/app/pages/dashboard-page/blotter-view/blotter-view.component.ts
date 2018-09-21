4c1d2cc0299624dd3b57a411f42b54a0c60ac83d
Dana Munteanu <dana.maria.munteanu@gmail.com>
2018-09-21 07:06:56 +0300

    Updated design and added sorting functionality for blotter

--- App/ui/src/app/pages/dashboard-page/blotter-view/blotter-view.component.ts
+++ App/ui/src/app/pages/dashboard-page/blotter-view/blotter-view.component.ts
@@ -7,122 +7,178 @@ import { Transaction } from 'src/app/models/transaction';
   styleUrls: ['./blotter-view.component.css']
 })
 export class BlotterViewComponent implements OnInit {
-  tranzactions: Transaction[] = [
+
+  private sorted = false;
+
+  transactions: Transaction[] = [
     {
       dealId: 1234,
       username: "Catalin Popescu",
-      CCY: "USD",
-      notional: "1000000",
+      CCY: "USD/EUR",
+      rate: "0.86",
+      action: "SELL",
+      notional: 1000000,
       tenor: "1M",
       date: 1536606799000
     },
     {
       dealId: 212,
       username: "Bogdan Pascu",
-      CCY: "USD",
-      notional: "1000000",
+      CCY: "USD/EUR",
+      rate: "0.15",
+      action: "BUY",
+      notional: 1000000,
       tenor: "1M",
       date: 1536606771000
     },
     {
       dealId: 34,
       username: "George Popescu",
-      CCY: "EUR",
-      notional: "1000000",
+      CCY: "EUR/USD",
+      rate: "0.63",
+      action: "SELL",
+      notional: 1000000,
       tenor: "3M",
       date: 1536605766000
     },
     {
       dealId: 455,
       username: "Andrei Nare",
-      CCY: "USD",
-      notional: "1000000",
+      CCY: "USD/EUR",
+      rate: "1.11",
+      action: "SELL",
+      notional: 1000000,
       tenor: "1M",
       date: 1536605766000
     },
     {
       dealId: 55,
       username: "George Barbu",
-      CCY: "RON",
-      notional: "1000000",
+      CCY: "RON/EUR",
+      rate: "0.46",
+      action: "SELL",
+      notional: 1000000,
       tenor: "1M",
       date: 1536605766000
     },
     {
       dealId: 3133,
       username: "Maria Munteanu",
-      CCY: "USD",
-      notional: "1000000",
+      CCY: "USD/GBP",
+      rate: "1.04",
+      action: "BUY",
+      notional: 1000000,
       tenor: "1M",
       date: 1536605754000
     },
     {
       dealId: 3123,
       username: "Mihai Nedelcu",
-      CCY: "EUR",
-      notional: "1000000",
+      CCY: "EUR/GBP",
+      rate: "0.86",
+      action: "BUY",
+      notional: 1000000,
       tenor: "1M",
       date: 1536605754000
     },
     {
       dealId: 4523,
       username: "Dan Purcarete",
-      CCY: "USD",
-      notional: "1000000",
+      CCY: "USD/GBP",
+      rate: "0.31",
+      action: "SELL",
+      notional: 1000000,
       tenor: "1M",
       date: 1536605754000
     },
     {
       dealId: 123,
       username: "Ileana Geo",
-      CCY: "RON",
-      notional: "1000000",
+      CCY: "RON/USD",
+      rate: "0.67",
+      action: "SELL",
+      notional: 1000000,
       tenor: "3M",
       date: 1536605747000
     },
     {
       dealId: 12,
       username: "Dana Popescu",
-      CCY: "EUR",
-      notional: "1000000",
+      CCY: "EUR/USD",
+      rate: "0.61",
+      action: "SELL",
+      notional: 1000000,
       tenor: "1M",
       date: 1536605743000
     },
     {
       dealId: 1,
       username: "George Nicolae",
-      CCY: "USD",
-      notional: "234324",
+      CCY: "USD/GBP",
+      rate: "1.23",
+      action: "SELL",
+      notional: 234324,
       tenor: "1M",
       date: 1536605738000
     },
     {
       dealId: 1234,
       username: "Piti Popescu",
-      CCY: "USD",
-      notional: "10050000",
+      CCY: "USD/RON",
+      rate: "1.01",
+      action: "BUY",
+      notional: 10050000,
       tenor: "1M",
       date: 1536605728000
     },
     {
       dealId: 55,
       username: "Adriana Popescu",
-      CCY: "USD",
-      notional: "1234",
+      CCY: "USD/EUR",
+      rate: "0.12",
+      action: "BUY",
+      notional: 1234,
       tenor: "1M",
       date: 1536605724000
     },
     {
       dealId: 5234,
       username: "George Marcu",
-      CCY: "USD",
-      notional: "55523",
+      CCY: "USD/RON",
+      rate: "0.45",
+      action: "BUY",
+      notional: 555.23,
       tenor: "3M",
       date: 1536604812000
     },
-  ]
+  ];
+
+  filter = {};
+
+  currencies = this.transactions
+                .map(transaction => transaction.CCY)
+                .filter((x, i, a) => x && a.indexOf(x) === i)
+  
+  usernames = this.transactions
+                .map(transaction => transaction.username)
+                .filter((x, i, a) => x && a.indexOf(x) === i)
+
   constructor() { }
   ngOnInit() {
+    
+  }
+
+  sortBy(sortCriteria: any): void {
+    this.transactions.sort((a:any, b:any) => {
+      if (a[sortCriteria] < b[sortCriteria]) {
+        return this.sorted ? 1 : -1;
+      }
+      if (a[sortCriteria] > b[sortCriteria]) {
+        return this.sorted ? -1 : 1;
+      }
+      return 0;
+    })
+    this.sorted = !this.sorted;
   }
 
 }
