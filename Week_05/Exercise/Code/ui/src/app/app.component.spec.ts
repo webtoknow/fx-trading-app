f6bbcdf9b8e2d11a7c75d59e1ade8a6424dfcd79
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2024-10-03 14:23:43 +0300

    Week 5 - Code UI moved to modules

--- Week_05/Exercise/Code/ui/src/app/app.component.spec.ts
+++ Week_05/Exercise/Code/ui/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ui' title`, () => {
+  it(`should have as title 'ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ui');
