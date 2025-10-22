9b2dca4ebbb38e850351bb14f52dfe54c3f0e4b2
alexsch86 <schiaucualex@gmail.com>
2025-10-22 22:50:25 +0300

    - split the user admin initialisation script into DDL and DML scripts - separate DDL commands in init scripts and DML in data scripts - create 2 separate containers for the 2 databases: users and fxtrading - eliminate the database and user creation commands from the initialisation scripts, since this is handled by docker compose POSTGRES environment variables

--- App/fx-trading/database_setup/init.sql
+++ App/fx-trading/database_setup/init.sql
@@ -1,7 +1,20 @@
 
-create database fxtrading;
+--create database fxtrading;
 
-CREATE USER fxuser WITH ENCRYPTED PASSWORD 'password';
+create table transactions (
+  id bigserial primary key,
+  username varchar(50) not null,
+  primary_ccy varchar(20) not null,
+  secondary_ccy varchar(20) not null,
+  rate bigint not null,
+  action varchar(20) not null,
+  notional bigint not null,
+  tenor varchar(20) not null,
+  date timestamp not null default now()
+);
+
+
+--CREATE USER fxuser WITH ENCRYPTED PASSWORD 'password';
 GRANT ALL PRIVILEGES ON DATABASE fxtrading TO fxuser;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO fxuser;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO fxuser;
