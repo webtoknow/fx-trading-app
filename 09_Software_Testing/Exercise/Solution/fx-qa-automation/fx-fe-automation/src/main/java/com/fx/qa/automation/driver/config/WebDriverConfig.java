c0b0c4922a0073ce038185e19d1e1f7466c7db65
banu <banu_maryus@yahoo.com>
2025-12-01 12:14:52 +0200

    Deleted old framework. Created new one that makes use of springboot

--- 09_Software_Testing/Exercise/Solution/fx-qa-automation/fx-fe-automation/src/main/java/com/fx/qa/automation/driver/config/WebDriverConfig.java
+++ 09_Software_Testing/Exercise/Solution/fx-qa-automation/fx-fe-automation/src/main/java/com/fx/qa/automation/driver/config/WebDriverConfig.java
@@ -6,6 +6,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 @LazyConfiguration
@@ -20,5 +26,31 @@ public class WebDriverConfig {
         options.addArguments("--guest");
         options.addArguments("--disable-search-engine-choice-screen");
         return new ChromeDriver(options);
+    }
+
+    @ThreadScopeBean
+    @ConditionalOnProperty(name = "application.browser", havingValue = "edge")
+    public WebDriver edgeDriver() {
+        WebDriverManager.edgedriver().setup();
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--guest");
+        return new EdgeDriver(options);
+    }
+
+    @ThreadScopeBean
+    @ConditionalOnProperty(name = "application.browser", havingValue = "firefox")
+    public WebDriver firefoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--start-maximized");
+        return new FirefoxDriver(options);
+    }
+
+    @ThreadScopeBean
+    @ConditionalOnProperty(name = "application.browser", havingValue = "safari")
+    public WebDriver safariDriver() {
+        SafariOptions options = new SafariOptions();
+        return new SafariDriver(options);
     }
 }
